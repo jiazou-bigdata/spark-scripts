af241bd97e1538693902e22db5e9b7f697239cbc
jiazou-bigdata <jacquelinezou@gmail.com>
2020-03-08 21:06:13 +0000

    Refine stopAll.sh to be more generic

--- stopAll.sh
+++ stopAll.sh
@@ -1,5 +1,9 @@
+user=$1
+key=$2
+serverlist=$3
+
 $HADOOP_HOME/sbin/stop-all.sh
 $SPARK_HOME/sbin/stop-all.sh
 $SPARK_HOME/sbin/stop-history-server.sh
-~/sshWorkers.sh ubuntu ~/ObjectQueryModel/conf/aaa10.pem ~/serverlist 'rm -rf /mnt/spark'
-~/sshWorkers.sh ubuntu ~/ObjectQueryModel/conf/aaa10.pem ~/serverlist 'rm -rf /mnt/dfs'
+~/sshWorkers.sh $user $key $serverlist 'rm -rf /mnt/spark'
+~/sshWorkers.sh $user $key $serverlist 'rm -rf /mnt/dfs'
